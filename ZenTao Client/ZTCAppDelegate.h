a98e921697e4e74b560f554429c98cc0102d34ec
Puttin Wong <yzwang.nj@gmail.com>
2013-03-17 16:10:42 +0800

    Initial Commit

---
+++ ZenTao Client/ZTCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZTCAppDelegate.h
+//  ZenTao Client
+//
+//  Created by Puttin Wong on 3/17/13.
+//  Copyright (c) 2013 Puttin Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZTCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZenTao Client/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZenTao Client
+//
+//  Created by Puttin Wong on 3/17/13.
+//  Copyright (c) 2013 Puttin Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZTCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZTCAppDelegate class]));
+    }
+}
